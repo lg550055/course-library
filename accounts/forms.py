5e30fe59a4dbdc4695e9d3830c7bbee574848af1
Polo <polospam@outlook.com>
2022-05-29 22:00:23 -0500

    Moves templates and static dirs to root

---
+++ accounts/forms.py
@@ -0,0 +1,19 @@
+from dataclasses import fields
+from pyexpat import model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+  class Meta:
+    model = CustomUser
+    fields = ('username', 'email')
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+  class Meta:
+    model = CustomUser
+    fields = ('username', 'email')
--- course_lib_project/settings.py
+++ course_lib_project/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local
-    'catalog'
+    'catalog',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'course_lib_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,7 +110,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
