5841a67deacaac216e3fa6af9a1a1ee6e52e9319
lg550055 <polospam@outlook.com>
2022-05-30 14:46:43 -0500

    Adds custom user model

--- course_lib_project/urls.py
+++ course_lib_project/urls.py
@@ -20,8 +20,9 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    # path('accounts/', include('django.contrib.auth.urls'))
 ]
