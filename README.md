c59824b535ce26f2bba5d2796b49d5cd7fbab179
lg550055 <lg550055@hotmail.com>
2022-07-28 17:16:58 -0500

    Updates README

--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 
 Django full-stack site hosting our course catalog.
 
-### Features
+The site's intended use is for staff to manage the catalog.
+
+Users can sign up / log in to have full CRUD functionality.
+
+### How to run locally
+
+- Create a virtual environment
+- Install dependencies (requirements.txt)
+- Run: python manage.py runserver
+
+---
+
+### Technical Features
 
 - Custom CSS
 - Gunicorn server
@@ -11,3 +23,14 @@ Django full-stack site hosting our course catalog.
 - Environment variables
 - Full CRUD funtionality
 - Cloud Postgres database
+
+### Design decisions
+
+- Uses Django generic views
+- Uses Django generic forms
+
+### Areas for improvement
+
+- Use custom user sign up and log in forms
+- Use is owner or read only permissions
+- Add email verification
--- course_lib_project/settings.py
+++ course_lib_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'course-lib.herokuapp.com']
 
@@ -88,12 +88,9 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    # {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
 
 # Internationalization
